120d5e348905ec9892e86f5c43fccb3fd7d5a820
Hassan-Jaber <hassanggg098@gmail.com>
2023-02-18 17:20:17 +0300

    upload-project-tomoh relate #2

--- js/script.js
+++ js/script.js
@@ -12,75 +12,76 @@ jQuery(function ($) {
 	/*  Fixed header
 	/* ----------------------------------------------------------- */
 	$(window).on('scroll', function () {
+    // fixedHeader on scroll
+    function fixedHeader() {
+      var headerTopBar = $(".top-bar").outerHeight();
+      var headerOneTopSpace = $(".header-one .logo-area").outerHeight();
 
-		// fixedHeader on scroll
-		function fixedHeader() {
-			var headerTopBar = $('.top-bar').outerHeight();
-			var headerOneTopSpace = $('.header-one .logo-area').outerHeight();
+      var headerOneELement = $(".header-one .site-navigation");
+      var headerTwoELement = $(".header-two .site-navigation");
 
-			var headerOneELement = $('.header-one .site-navigation');
-			var headerTwoELement = $('.header-two .site-navigation');
+      if ($(window).scrollTop() > headerTopBar + headerOneTopSpace) {
+        $(headerOneELement).addClass("navbar-fixed");
+        $(".header-one").css("margin-bottom", headerOneELement.outerHeight());
+      } else {
+        $(headerOneELement).removeClass("navbar-fixed");
+        $(".header-one").css("margin-bottom", 0);
+      }
+      if ($(window).scrollTop() > headerTopBar) {
+        $(headerTwoELement).addClass("navbar-fixed");
+        $(".header-two").css("margin-bottom", headerTwoELement.outerHeight());
+      } else {
+        $(headerTwoELement).removeClass("navbar-fixed");
+        $(".header-two").css("margin-bottom", 0);
+      }
+    }
+    fixedHeader();
 
-			if ($(window).scrollTop() > headerTopBar + headerOneTopSpace) {
-				$(headerOneELement).addClass('navbar-fixed');
-				$('.header-one').css('margin-bottom', headerOneELement.outerHeight());
-			} else {
-				$(headerOneELement).removeClass('navbar-fixed');
-				$('.header-one').css('margin-bottom', 0);
-			}
-			if ($(window).scrollTop() > headerTopBar) {
-				$(headerTwoELement).addClass('navbar-fixed');
-				$('.header-two').css('margin-bottom', headerTwoELement.outerHeight());
-			} else {
-				$(headerTwoELement).removeClass('navbar-fixed');
-				$('.header-two').css('margin-bottom', 0);
-			}
-		}
-		fixedHeader();
+    // ========================================== Count Up ======================================================
+    // Count Up
+    function counter() {
+      var oTop;
+      if ($(".counterUp").length !== 0) {
+        oTop = $(".counterUp").offset().top - window.innerHeight;
+      }
+      if ($(window).scrollTop() > oTop) {
+        $(".counterUp").each(function () {
+          var $this = $(this),
+            countTo = $this.attr("data-count");
+          $({
+            countNum: $this.text(),
+          }).animate(
+            {
+              countNum: countTo,
+            },
+            {
+              duration: 1000,
+              easing: "swing",
+              step: function () {
+                $this.text(Math.floor(this.countNum));
+              },
+              complete: function () {
+                $this.text(this.countNum);
+              },
+            }
+          );
+        });
+      }
+    }
+    counter();
 
-
-		// Count Up
-		function counter() {
-			var oTop;
-			if ($('.counterUp').length !== 0) {
-				oTop = $('.counterUp').offset().top - window.innerHeight;
-			}
-			if ($(window).scrollTop() > oTop) {
-				$('.counterUp').each(function () {
-					var $this = $(this),
-						countTo = $this.attr('data-count');
-					$({
-						countNum: $this.text()
-					}).animate({
-						countNum: countTo
-					}, {
-						duration: 1000,
-						easing: 'swing',
-						step: function () {
-							$this.text(Math.floor(this.countNum));
-						},
-						complete: function () {
-							$this.text(this.countNum);
-						}
-					});
-				});
-			}
-		}
-		counter();
-
-
-		// scroll to top btn show/hide
-		function scrollTopBtn() {
-			var scrollToTop = $('#back-to-top'),
-				scroll = $(window).scrollTop();
-			if (scroll >= 50) {
-				scrollToTop.fadeIn();
-			} else {
-				scrollToTop.fadeOut();
-			}
-		}
-		scrollTopBtn();
-	});
+    // scroll to top btn show/hide
+    function scrollTopBtn() {
+      var scrollToTop = $("#back-to-top"),
+        scroll = $(window).scrollTop();
+      if (scroll >= 50) {
+        scrollToTop.fadeIn();
+      } else {
+        scrollToTop.fadeOut();
+      }
+    }
+    scrollTopBtn();
+  });
 
 
 	$(document).ready(function () {
@@ -270,3 +271,4 @@ jQuery(function ($) {
 
 
 });
+// ========================================== Count Up ======================================================
